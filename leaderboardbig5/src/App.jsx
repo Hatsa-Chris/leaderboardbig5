5dda96a23f56765786d528b56cbd13fbffe185b3
Hatsa-Chris <chris@hatsa.com>
2022-08-08 15:11:13 +0200

    Over naar Vite

---
+++ leaderboardbig5/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl text-teal-500 font-bold">Dit is een leaderboard</h1>
+  )
+}
+
+export default App
